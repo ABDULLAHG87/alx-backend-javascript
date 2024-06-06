51d41150effa52646ccc8036504da4197861f4c6
Abdullahi <hakeemabdullah87@gmail.com>
2024-06-06 08:59:30 +0000

    FEAT: add file 0-calcul.js, 0-calcul.test.js and package.json for task 0

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,23 @@
+const calculateNumber = require('./0-calcul.js');
+const assert = require('assert');
+
+describe('calculateNumber', () =>{
+    it('rounding of a', () => {
+	assert.equal(calculateNumber(13.45, 7), 20);
+	assert.equal(calculateNumber(17.55, 1), 19);
+	assert.equal(calculateNumber(1.05, 3), 4);
+    });
+
+    it('rounding of b', () => {
+	assert.equal(calculateNumber(7, 13.45), 20);
+	assert.equal(calculateNumber(1, 17.55), 19);
+	assert.equal(calculateNumber(3, 1.05), 4);
+    });
+
+    it('suming of a and b', () => {
+	assert.equal(calculateNumber(13.45, 7.6), 21);
+	assert.equal(calculateNumber(17.55, 1.02), 19);
+	assert.equal(calculateNumber(1.66, 7.89), 10);
+    });
+    
+})
