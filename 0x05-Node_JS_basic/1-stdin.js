eb366a544e36ee54992590983a98b80be8fa03b6
Abdullahi <hakeemabdullah87@gmail.com>
2024-06-08 01:34:33 +0000

    BUGFIX: fix newline error in task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdin.setEncoding('utf8');
 
-process.stdout.write('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
@@ -9,6 +9,6 @@ process.stdin.on('readable', () => {
   }
 });
 
-process.stdin.on('exit', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
